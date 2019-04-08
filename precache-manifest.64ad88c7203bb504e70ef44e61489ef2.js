6f79ee24de02929113383c5079b12995792025a4
mooohit <46820938+mooohit@users.noreply.github.com>
2019-04-08 18:50:02 +0530

    Add files via upload

---
+++ precache-manifest.64ad88c7203bb504e70ef44e61489ef2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2509ee9a2c2e29aa505c",
+    "url": "/static/css/main.b77db848.chunk.css"
+  },
+  {
+    "revision": "2509ee9a2c2e29aa505c",
+    "url": "/static/js/main.4c6803f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "88eba261bf4ade196cc9",
+    "url": "/static/js/2.9e94a8f2.chunk.js"
+  },
+  {
+    "revision": "2486740e1a1a27e4a48359355ab027e3",
+    "url": "/index.html"
+  }
+];
